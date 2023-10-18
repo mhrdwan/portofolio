753f0163f0175c6e95526a34117896e7d248c933
Mohamad Hasyim Ridwan <123049383+mhrdwan@users.noreply.github.com>
2023-10-18 18:27:15 +0700

    first commit

---
+++ src/app/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div className='flex items-center justify-between py-5 mb-4 text-zinc-500 dark:text-zinc-300 '>
+            <div className='fixed inset-0 z-10 h-screen transition-transform duration-200 transform scale-0 lg:scale-100 lg:h-fit lg:static backdrop-filter backdrop-blur-xl lg:backdrop-blur-0 lg:w-4/6'>
+                <ul>
+                    <li className='block mx-4 mr-10 font-normal lg:py-2 lg:px-4 lg:font-bold border-zinc-700 lg:m-0 lg:inline-block'>
+                        <a className='text-sm transition-all  hover:text-teal-300 text-teal-300 cursor-pointer'>About</a>
+                    </li>
+                    <li className='block mx-4 mr-10 font-normal lg:py-2 lg:px-4 lg:font-bold border-zinc-700 lg:m-0 lg:inline-block'>
+                        <a className='text-sm transition-all  hover:text-teal-300 text-teal-300  cursor-pointer'>Portofolio</a>
+                    </li>
+                    <li className='block mx-4 mr-10 font-normal lg:py-2 lg:px-4 lg:font-bold border-zinc-700 lg:m-0 lg:inline-block'>
+                        <a className='text-sm transition-all  hover:text-teal-300 text-teal-300  cursor-pointer'>Blog</a>
+                    </li>
+                </ul>
+            </div>
+            <div className='w-full text-xl lg:w-2/6 '>
+                <a className='font-bold justify-centerflex'>MHR</a>
+            </div>
+            <div className='flex items-center gap-4 p-2 transition-all border rounded-lg border-zinc-300 hover:border-gray-600'>
+                <button>Dark/Light</button>
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Navbar
