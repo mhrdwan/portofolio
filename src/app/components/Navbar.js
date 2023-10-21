e3658f567ed4112a4e65ca51befb7161a5d048f0
Mohamad Hasyim Ridwan <123049383+mhrdwan@users.noreply.github.com>
2023-10-22 01:52:32 +0700

    ridwan

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -44,7 +44,7 @@ function Navbar() {
 
     return (
 
-        <div className={`flex items-center justify-between py-5 mb-4 text-zinc-500 dark:text-zinc-300  ${hide === false && theme === "dark" ? `dark:bg-zinc-900 bg-white  transition-all duration-700 ease-in-out` : ""}  ${hide === false && theme === "dark" && angka <= 100 ? `dark:bg-transparent bg-white  transition-all duration-700 ease-in-out` : ""} ${hide === false && theme === "light" ? `dark:bg-zinc-900 bg-white  transition-all duration-700 ease-in-out` : ""} top-0 sticky z-50`} >
+        <div className={`flex items-center justify-between py-5 mb-4 text-zinc-500 dark:text-zinc-300  ${hide === false && theme === "dark" ? `dark:bg-zinc-900 bg-white  transition-all duration-700 ease-in-out` : ""}  ${hide === false && theme === "dark" && angka <= 5 ? `dark:bg-transparent bg-white  transition-all duration-700 ease-in-out` : ""} ${hide === false && theme === "light" ? `dark:bg-zinc-900 bg-white  transition-all duration-700 ease-in-out` : ""} top-0 sticky z-50`} >
             <div className={`fixed inset-0 z-10 h-screen transition-transform duration-200 transform ${scales} lg:scale-100 lg:h-fit lg:static backdrop-filter backdrop-blur-xl lg:backdrop-blur-0 lg:w-4/6`}>
                 <motion.nav
                     variants={{
