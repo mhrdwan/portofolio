48c71948c3d28d5fedbeba912626f8c2bd7c93c5
Mohamad Hasyim Ridwan <123049383+mhrdwan@users.noreply.github.com>
2023-10-30 07:38:55 +0700

    ridwan

--- src/app/portofolio/page.js
+++ src/app/portofolio/page.js
@@ -10,6 +10,7 @@ import CLI_Tiktok_Downloader from "../assets/IMG/CLI_Tiktok_Downloader.png"
 import CLI_Cek_Cuaca from "../assets/IMG/CLI_Cek_Cuaca.png"
 import porto_RajaCepat from "../assets/IMG/porto_RajaCepat.png"
 import porto_Eureka_Logistic from "../assets/IMG/porto_Eureka_Logistic.png"
+import API_Scraping_berita_CNN from "../assets/IMG/API_Scraping_berita_CNN.png"
 function Portofolio() {
 
 
@@ -96,6 +97,21 @@ function Portofolio() {
                                     <button className='px-1.5 py-0.5 text-xs rounded-lg bg-zinc-200 dark:bg-zinc-800 text-zinc-600 dark:text-zinc-400'>React JS</button>
                                     <button className='px-1.5 py-0.5 text-xs rounded-lg bg-zinc-200 dark:bg-zinc-800 text-zinc-600 dark:text-zinc-400'>Ant Design</button>
                                     <button className='px-1.5 py-0.5 text-xs rounded-lg bg-zinc-200 dark:bg-zinc-800 text-zinc-600 dark:text-zinc-400'>Bootstrap</button>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='p-4 transition-all duration-100 rounded-lg shadow-lg cursor-pointer lg:p-2 dark:shadow-none hover:bg-zinc-200 dark:hover:bg-zinc-800'>
+                            <div className='rounded-lg shadow-md'>
+                                <Image className='rounded-lg shadow-md h-44' height={150} width={500} src={API_Scraping_berita_CNN} alt='="porto_eureka_tracking' />
+                            </div>
+                            <div className='p-2'>
+                                <h1 className='text-sm font-semibold line-clamp-2 dark:text-white text-zinc-600'>API - Scraping Berita CNN </h1>
+                                <a className='flex mt-5 items-center gap-1 text-xs hover:text-teal-300  hover:cursor-not-allowed  text-white' >
+                                    <svg className='h-3 w-3' xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-link"><path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71" /><path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71" /></svg>
+                                    Private
+                                </a>
+                                <div className='flex flex-wrap gap-2 mt-4'>
+                                    <button className='px-1.5 py-0.5 text-xs rounded-lg bg-zinc-200 dark:bg-zinc-800 text-zinc-600 dark:text-zinc-400'>Node JS</button>
                                 </div>
                             </div>
                         </div>
