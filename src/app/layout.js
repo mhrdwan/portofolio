993b0a12224b094c30025176e89310ee3d236aae
mhrdwan <mohamad.ridwan@binus.ac.id>
2024-12-15 21:37:17 +0700

    push

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Providers from "./Providers";
+import Head from "next/head";
 
 const inter = Poppins({
   subsets: ["latin"],
@@ -30,6 +31,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </head>
       <body className={inter.className}>
         <Providers>{children}</Providers>
       </body>
